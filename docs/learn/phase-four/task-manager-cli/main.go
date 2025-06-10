40146b6a915f8f7c2273f051b382a52d97c2fbde
itzzjb <desilvabethmin@gmail.com>
2025-06-10 11:42:25 +0530

    learning-phase-four

---
+++ docs/learn/phase-four/task-manager-cli/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"github.com/spf13/viper"
+)
+
+func initConfig() {
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(".")
+
+	if err := viper.ReadInConfig(); err != nil {
+		fmt.Printf("Error reading config file, %s\n", err)
+	} else {
+		fmt.Println("Config file loaded successfully")
+	}
+}
+
+func init() {
+	initConfig()
+}
+
+func main() {
+	databaseEngine := viper.GetString("database.engine")
+	databaseUser := viper.GetString("database.user")
+	databasePassword := viper.GetString("database.password")
+	databaseHost := viper.GetString("database.host")
+	databasePort := viper.GetInt("database.port")
+
+	if databaseEngine == "" || databaseUser == "" || databasePassword == "" || databaseHost == "" || databasePort == 0 {
+		fmt.Println("Missing required database configuration")
+		return
+	} else {
+		fmt.Println("Database URL:", fmt.Sprintf("%s://%s:%s@%s:%d", databaseEngine, databaseUser, databasePassword, databaseHost, databasePort))
+	}
+
+}
